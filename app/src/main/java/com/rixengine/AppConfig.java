f8470914ab1fe6d7b017069af225d8d6ab175f10
rixenginesdk <rix-sdk@rixengine.com>
2025-10-16 15:22:24 +0800

    Update AppConfig.java
    fix bugs

--- app/src/main/java/com/rixengine/AppConfig.java
+++ app/src/main/java/com/rixengine/AppConfig.java
@@ -15,28 +15,7 @@ public class AppConfig {
     public static String ALX_REWARD_VIDEO_AD_PID = "201306";//reward video AD id
     public static String ALX_INTERSTITIAL_AD_PID = "201299";//interstitial AD id
     public static String ALX_SPLASH_AD_PID = "201307";//splash AD id
-
-//    appid:102721
-//    sid:38596
-//    token:6123a35f2a3d8a00ff18a3659febae7b
-//    host:https://vosaco.svr.rixengine.com/rtb
-//
-//    Reward unitid:203358
-//    inters unitid:203359
-
-    // 开发者 test 配置
-//    public static String ALX_HOST = "https://vosaco.svr.rixengine.com/rtb";
-//    public static String ALX_SID = "38596"; // sid
-//    public static String ALX_APP_ID = "102721"; // app_id
-//    public static String ALX_TOKEN = "6123a35f2a3d8a00ff18a3659febae7b"; // token
-//
-//    public static String ALX_BANNER_AD_PID = "201379"; //banner AD id
-//    public static String ALX_NATIVE_AD_PID = "201380";//native AD id
-//    public static String ALX_REWARD_VIDEO_AD_PID = "203358";//reward video AD id
-//    public static String ALX_INTERSTITIAL_AD_PID = "203359";//interstitial AD id
-//    public static String ALX_SPLASH_AD_PID = "172140";//splash AD id
-
-
+ 
     //topon 平台广告配置信息
     public static final String TOPON_KEY = "aa10e0b266ae76d73f3185ec7a58a211c";
     public static final String TOPON_APP_ID = "h67dcda957f660";
@@ -46,26 +25,11 @@ public class AppConfig {
     public static final String TOPON_NATIVE_PID = "n67dcdbef86db6";
     public static final String TOPON_SPLASH_PID = "b611335c2f2678";
 
-    //ocr 中介广告位：
-//    appid: ca-app-pub-4920936244266233~1192954605
-//    banner: ca-app-pub-4920936244266233/8031693522
-//    native:  ca-app-pub-4920936244266233/5760753407
-//    RD: ca-app-pub-4920936244266233/8770060126
-//    init: ca-app-pub-4920936244266233/2943018374
-    //Google AdMob 平台广告配置信息
-//    public static final String ADMOB_APP_ID = "ca-app-pub-4920936244266233~6784498952";
-//    public static final String ADMOB_BANNER_ID = "ca-app-pub-4920936244266233/6225547012";
-//    public static final String ADMOB_INTERSTITIAL_ID = "ca-app-pub-4920936244266233/8903681248";
-//    public static final String ADMOB_REWARD_ID = "ca-app-pub-4920936244266233/3966763924";
-//    public static final String ADMOB_NATIVE_ID = "ca-app-pub-4920936244266233/3307739637";
-
     public static final String ADMOB_APP_ID = "ca-app-pub-4885562717451614~4848824427";
-    // public static final String ADMOB_BANNER_ID = "ca-app-pub-4885562717451614/9675235882";
+    public static final String ADMOB_BANNER_ID = "ca-app-pub-4885562717451614/9675235882";
     public static final String ADMOB_INTERSTITIAL_ID = "ca-app-pub-4885562717451614/9795659657";
     public static final String ADMOB_REWARD_ID = "ca-app-pub-4885562717451614/1126832141";
     public static final String ADMOB_NATIVE_ID = "ca-app-pub-4885562717451614/2332102605";
-
-
 
     //TradPlus 平台广告配置信息
     public static final String TRAD_PLUS_APP_ID = "9851676EA9E78F40898FD8128AFF98CD";
@@ -77,7 +41,6 @@ public class AppConfig {
 
     //Iron Source 平台广告配置信息
     public static final String IRON_SOURCE_APP_KEY = "1c1b6c5f5";
-
 
     //Max 平台广告配置信息
     public static final String MAX_APP_KEY = "";
